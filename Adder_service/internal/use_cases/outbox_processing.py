9f1996ee5a4e2ec29f212328766c40107f3a80f7
refaat <theonerefaat@gmail.com>
2025-05-01 02:16:09 +0300

    completing Outbox pattern

--- Adder_service/internal/use_cases/outbox_processing.py
+++ Adder_service/internal/use_cases/outbox_processing.py
@@ -3,6 +3,8 @@ from internal.adapters.outbox_repository import OutBoxRepository
 from internal.entities.outbox_message import Outbox
 from internal.utils.logger import setup_logger
 import threading,time
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import asyncio
 logger = setup_logger('outbox.log')
 class OutBoxProcessor:
     def __init__(self,repository:OutBoxRepository,publisher:RabbitMQPublisher,interval:float,clustersize:int):
@@ -15,14 +17,22 @@ class OutBoxProcessor:
     
     def process_messages(self):
         logger.info("Fetching pending messages from outbox")
-        
         outboxes = self.repository.GetPendingMessages(self.clustersize)
-        
-        if(len(outboxes)== 0):
-            logger.info("There is no outbox Messages to be processed")
-        
-        for outbox in outboxes:
-            ob = Outbox()
-         # to be completed  
+        if not outboxes:
+            logger.info("There are no outbox messages to be processed")
+            return
+
+        def handle_message(ob):
+            try:
+                self.publisher.publish_message("sums", ob.value)
+                self.repository.MarkAsProcessed(ob.id)
+            except Exception as e:
+                logger.error(f"Error processing message ID {ob.id}: {e}")
+
+        with ThreadPoolExecutor(max_workers=5) as executor:
+            futures = [executor.submit(handle_message, ob) for ob in outboxes]
+            for future in as_completed(futures):
+                future.result()
+
     
     
